2a732236dfde3858fb490b9a75ab75a780d5b021
LoickMury <loick.mury@epitech.eu>
2020-03-07 14:10:53 +0900

    Fusion reussi des pages de connexion de fonctionnel et design

--- Design/Screen/SignIn.js
+++ Design/Screen/SignIn.js
@@ -28,25 +28,125 @@ export default class SignIn extends React.Component {
         })
     }
 
-    handleSubmit() {
-        let data = JSON.stringify({
-            "user": {
-                "email": this.state.email,
-                "password": this.state.password,
-            }
-        });
-            Axios.post(Global.IPServer + "/user/local/login",
-            data,
-            { headers: { "Content-Type": "application/json" } })
-            .then((response) => {
-                if (response.status == 200 || response.status == 201) {
-                    this._storeData("userRef", response.data);
-                    this.props.navigation.navigate('Home');
-                }
-            }).catch(function (error) {
-                // console.error(error);
-            })
+    toggleSignIn() {
+        if (this.state.email.length < 1) {
+          alert('Please enter an email address.');
+          return(84);
         }
+        if (this.state.password.length < 1) {
+          alert('Please enter a password.');
+          return(84);
+        }
+    
+        var data = {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: this.state.email,
+            password: this.state.password,
+          }),
+        }
+        
+        fetch('http://40.85.113.74:3000/auth/signin', data).then((res) => res.json())
+          .then((resjson) => {
+            if (resjson.success === true) {
+              var str = resjson.token;
+              global.token = str.slice(4, str.length);
+              this.props.navigation.navigate('Home');
+              //this.getUserInfos();
+            }
+            else {
+              alert(resjson.error);
+              return;
+            }
+          })
+          .catch((error) => {
+            alert(error);
+          })
+      }
+    
+      getUserInfos() {
+        var data = {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            token: global.token,
+          }),
+        }
+    
+        fetch('http://40.85.113.74:3000/auth/getuserinfos', data).then((res) => res.json())
+          .then((resjson) => {
+            if (resjson.success === true) {
+              global.name = resjson.name;
+              global.email = resjson.email
+              global.date = resjson.birthdate.split('T')[0];
+              this.setState({ email: '', password: '' });
+              this.getCarInfos();
+            }
+            else {
+              alert(resjson.error);
+              return;
+            }
+          });
+      }
+    
+      getCarInfos() {
+        var data = {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            token: global.token,
+          }),
+        }
+        fetch('http://40.85.113.74:3000/data/getvehiculeinfo', data).then((res) => res.json())
+          .then((resjson) => {
+            if (resjson.success === true) {
+              global.speed = resjson.speed;
+              global.fuel = resjson.fuel;
+              global.lat = resjson.latitude;
+              global.long = resjson.longitude;
+              this.setState({ email: '', password: '' });
+              NavigationService.navigate('MainHome');
+            }
+            else {
+              alert(resjson.error);
+              return;
+            }
+          });
+      }
+
+    // handleSubmit() {
+    //     let data = JSON.stringify({
+    //         "user": {
+    //             "email": this.state.email,
+    //             "password": this.state.password,
+    //         }
+    //     });
+    //         Axios.post("http://40.85.113.74:3000/auth/signin",
+    //         data,
+    //         { headers: { Accept: 'application/json', "Content-Type": "application/json" } })
+    //         .then((response) => {
+    //             if (response.status == 200 || response.status == 201) {
+    //                 //this._storeData("userRef", response.data);
+    //                 this.props.navigation.navigate('Home');
+    //             }
+    //             else {
+    //                 console.log(response.error);
+    //                 return;
+    //             }
+    //         }).catch(function (error) {
+    //             console.log(error);
+    //         })
+    //     }
         
     _storeData = async (key, value) => {
         try {
@@ -90,7 +190,7 @@ export default class SignIn extends React.Component {
                             onChangeText={(text) => this.setPassword(text)}>
                         </TextInput>
                         <Button
-                            onPress={() => this.props.navigation.navigate('Home')}
+                            onPress={() => this.toggleSignIn()/*this.handleSubmit()*//*this.props.navigation.navigate('Home')*/}
                             title="Sign In"
                             buttonStyle={styles.Button}>
                         </Button>
