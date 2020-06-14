808dfb401c1fefa9a8a3e82b8b53e0d88a077c3c
Azriel <thomas.domine-@epitech.eu>
2020-06-14 21:26:57 +0200

    ajout d'un système de notification

--- Design/index.js
+++ Design/index.js
@@ -3,10 +3,14 @@
  */
 
 import {AppRegistry} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import App from './App';
 import {name as appName} from './app.json';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
-// AppRegistry.registerComponent(appName, () => App);
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
+
