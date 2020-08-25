4e2c747e9eae1dc2e6266db96dd8063c6c7ae78d
Azriel <thomas.domine-@epitech.eu>
2020-08-25 21:45:13 +0200

    premier jet avec correct labels

--- Design/Screen/Battery.js
+++ Design/Screen/Battery.js
@@ -17,16 +17,16 @@ import {
 
 import NavBar from '../Tools/NavBar';
 
-export default class Fuel extends React.Component {
+export default class Battery extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
             fuel: 0,
-            value_actual_fuel: 50,
-            last_fill_fuel: new Date("2020-07-10"),
-            distance_left: 200,
-            consomation_per_kilometer_max: 10.4, 
-            consomation_per_kilometer: 7.5,
+            value_actual_battery: 65,
+            time_remaining: "12h34",
+            generation: 5,
+            consomation_max: 5, 
+            consomation: 3,
         }
     }
 
@@ -52,7 +52,7 @@ export default class Fuel extends React.Component {
             <NavBar onPushButton={() => this.props.navigation.openDrawer()}/>
             <View style={{width: widthPercentage("100%"), marginTop: heightPercentage("3%")}}>
                 <Text style={styles.Title}>
-                    Fuel
+                    Battery
                 </Text>
             </View>
             <View style={{flexDirection: 'row', marginTop: heightPercentage("3%")}}>
@@ -60,7 +60,7 @@ export default class Fuel extends React.Component {
                     <GaugeProgress style={styles.AnimatedGaugeProgress}
                         size={widthPercentage('40%')}
                         width={9}
-                        fill={this.state.value_actual_fuel}
+                        fill={this.state.value_actual_battery}
                         rotation={90}
                         cropDegree={90}
                         tintColor="#2c84cc"
@@ -69,27 +69,27 @@ export default class Fuel extends React.Component {
                         stroke={[2, 2]}
                         strokeCap="circle">
                             <View style={styles.ViewValue}>
-                                <Text style={styles.ValueActualVolume}>{this.state.value_actual_fuel}%</Text>
+                                <Text style={styles.ValueActualVolume}>{this.state.value_actual_battery}%</Text>
                             </View>
                     </GaugeProgress>
                 </View>
                 <View style={{width: widthPercentage("50%"), marginLeft: heightPercentage("2.5%")}}>
-                        <Text style={styles.Text}>{"Last refueling:\n" + this.formatDate(this.state.last_fill_fuel)}</Text>
-                        <Text style={styles.Text}>{"Remaining distance:\n" + this.state.distance_left + "km"}</Text>
+                        <Text style={styles.Text}>{"Time remaining:\n" + this.state.time_remaining}</Text>
+                        <Text style={styles.Text}>{"Generated power:\n" + this.state.generation + "kw/H"}</Text>
                 </View>
             </View>
-                <Text style={{color: "#FFFFFF", fontSize: 15, marginTop: heightPercentage("5%"), marginLeft: heightPercentage("4%")}}>{"Fuel consumption (L/100km):\n"}</Text>
+                <Text style={{color: "#FFFFFF", fontSize: 15, marginTop: heightPercentage("5%"), marginLeft: heightPercentage("4%")}}>{"Electicity consumption (Kw/24h):\n"}</Text>
             <View style={{flexDirection: 'row'}}>
                 <Text style={{color: "#FFFFFF", fontSize: 15, marginLeft: widthPercentage("14%")}}>{"0"}</Text>
-                <Text style={{color: "#FFFFFF", fontSize: 15, marginLeft: widthPercentage("66%")}}>{this.state.consomation_per_kilometer_max}</Text>
+                <Text style={{color: "#FFFFFF", fontSize: 15, marginLeft: widthPercentage("66%")}}>{this.state.consomation_max}</Text>
             </View>
             <View style={{width: widthPercentage("70%"), marginLeft: widthPercentage("15%")}}>
                 <Progress.Bar style={{marginTop: heightPercentage("2%")}} borderWidth={0} color='rgba(44,132,204,1)' unfilledColor="#C2d5ef" 
-                progress={this.state.consomation_per_kilometer/this.state.consomation_per_kilometer_max} height={9} width={widthPercentage("70%")}/>
+                progress={this.state.consomation/this.state.consomation_max} height={9} width={widthPercentage("70%")}/>
                 <Text style={{color: "#FFFFFF", fontSize: 15, 
-                    marginLeft:(widthPercentage((70 * this.state.consomation_per_kilometer/this.state.consomation_per_kilometer_max).toString() - 2  + "%")),
+                    marginLeft:(widthPercentage((70 * this.state.consomation/this.state.consomation_max).toString() - 2  + "%")),
                     marginTop: heightPercentage("2%")}}>
-                    {this.state.consomation_per_kilometer}</Text>
+                    {this.state.consomation}</Text>
             </View>
             <View>
                 <LineChart
@@ -100,30 +100,30 @@ export default class Fuel extends React.Component {
                     datasets: [
                         {
                         data: [
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
-                            Math.random() * 50,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
+                            Math.random() * 10,
                         ]
                         }
                     ]
                     }}
                     width={widthPercentage("96%")} // from react-native
                     height={heightPercentage('33%')}
-                    yAxisLabel=""
-                    yAxisSuffix="L"
+                    yAxisLabel="H"
+                    yAxisSuffix="KW"
                     yAxisInterval={1}
                     xAxisInterval={10} // optional, defaults to 1
                     chartConfig={{
